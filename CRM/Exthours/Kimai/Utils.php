4d1b24777010dc1273bda4439b205278aa485c68
lucky091588 <lucky091588@gmail.com>
2021-04-25 06:53:48 +0800

    Add project parameter in getKimaiUpdatesData function and rewrite Getkimaiupdates api call

--- CRM/Exthours/Kimai/Utils.php
+++ CRM/Exthours/Kimai/Utils.php
@@ -146,7 +146,7 @@ class CRM_Exthours_Kimai_Utils {
   /**
    * Get kimai getUpdates api data
    */
-  public static function getKimaiUpdatesData() {
+  public static function getKimaiUpdatesData($projectId) {
     $apiKey = Civi::settings()->get('exthours_kimai_api_key');
 
     // Kimai Get Timesheet Method
@@ -154,6 +154,7 @@ class CRM_Exthours_Kimai_Utils {
       "method" => "getUpdates",
       "params" => array(
         $apiKey,
+        $projectId,
       ),
       "id" => "1",
       "jsonrpc" => "2.0",
